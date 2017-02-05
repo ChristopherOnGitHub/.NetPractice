6c7f467495075f0effcde1bd2d6a115beaedbdb2
Christopher Floyd <Christopher Floyd>
2017-02-04 18:28:39 -0600

    C# first app

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        while (true)
+        {
+            Console.WriteLine("Enter two numbers to add: ");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int sum = x + y;
+            Console.WriteLine("The sum of " + x + " & " + y + " is: " + sum);
+        }
+    }
+}
