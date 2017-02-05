9d0a0db64b1183f35126d9d33fd80a5f80cc86b5
Christopher Floyd <Christopher Floyd>
2017-02-04 18:49:29 -0600

    prime numbers c#

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Generate all prime numbers up to what number?");
+            int x = int.Parse(Console.ReadLine());
+            int j;
+
+            for (int i = 1; i <= x; i++)
+            {
+                int counter = 0;
+                for (j = 2; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
