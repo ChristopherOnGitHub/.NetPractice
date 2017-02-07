1473079667105262355dfe6e1a1f88bffe16d9a2
Christopher Floyd <Christopher Floyd>
2017-02-07 13:59:16 -0600

    fibonacci sequence in c#

---
+++ FibonacciC/FibonacciC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Generate fibonacci sequence up to what number?");
+            int x = int.Parse(Console.ReadLine());
+            int y = 1;
+            int sum = 0;
+            int z = 0;
+
+            while (sum < x)
+            {
+                sum = y+z;
+                if (sum<=x) {
+                    Console.WriteLine(sum);
+                }
+                z = y;
+                y = sum;
+            }
+
+        }
+    }
+}
